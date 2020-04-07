8d0d2d63ad1348f031b25f25605f1622970bd894
Johan Baaij <johanbaaij@gmail.com>
2020-04-07 12:31:11 +1000

    :tada: Initial commit

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+const INTERVAL_THRESHOLD = 1.5;
+
+export default class BpmCounter {
+  taps: Array<number>;
+
+  constructor(taps: number[] = []) {
+    this.taps = taps;
+  }
+
+  reset = () => {
+    this.taps = [];
+  };
+
+  tap = (ms: number = performance.now()) => {
+    if (this.intervalTooLong(ms)) {
+      return this.reset();
+    }
+    this.taps.push(ms);
+  };
+
+  get bpm() {
+    if (this.intervalAverageInMs) {
+      return 60000 / this.intervalAverageInMs;
+    }
+    return 0;
+  }
+
+  intervalTooLong(tapInMs: number) {
+    if (this.intervalAverageInMs === 0) return false;
+    const lastIntervalInMs = tapInMs - this.lastTapInMs;
+    return lastIntervalInMs > this.intervalAverageInMs * INTERVAL_THRESHOLD;
+  }
+
+  get lastTapInMs() {
+    return this.taps[this.taps.length - 1];
+  }
+
+  get intervalAverageInMs() {
+    if (this.taps.length < 2) {
+      return 0;
+    }
+    return this.intervalsInMs.reduce(
+      (total: number, amount: number, index: number, array: number[]) => {
+        total += amount;
+        if (index === array.length - 1) {
+          return total / array.length;
+        } else {
+          return total;
+        }
+      }
+    );
+  }
+
+  get intervalsInMs() {
+    const intervals = this.taps.map((stamp, index) => {
+      const nextStamp = this.taps[index + 1];
+      return nextStamp - stamp;
+    });
+    intervals.pop(); // Last entry is null
+    return intervals;
+  }
+}
---
+++ test/index.test.ts
@@ -0,0 +1,48 @@
+import BpmCounter from '../src';
+
+describe('BpmCounter', () => {
+  it('tap() increases array length by 1', () => {
+    const counter = new BpmCounter();
+    counter.tap();
+    expect(counter.taps.length).toEqual(1);
+    counter.tap();
+    expect(counter.taps.length).toEqual(2);
+  });
+
+  it('reset() resets the taps', () => {
+    const counter = new BpmCounter();
+    counter.tap();
+    counter.tap();
+    counter.tap();
+    expect(counter.taps.length).toEqual(3);
+    counter.reset();
+    expect(counter.taps.length).toEqual(0);
+  });
+
+  it('calculates the tap intervals', () => {
+    const counter = new BpmCounter([300, 400, 500, 600, 700]);
+    expect(counter.intervalsInMs.length).toBe(4);
+    expect(counter.intervalsInMs).toStrictEqual([100, 100, 100, 100]);
+  });
+
+  it('calculates interval average', () => {
+    const counter = new BpmCounter([500, 1000, 1500, 2000, 2500]);
+    expect(counter.intervalAverageInMs).toBe(500);
+  });
+
+  it('calculates BPM', () => {
+    const counter = new BpmCounter([0, 500]);
+    expect(counter.bpm).toBe(120);
+  });
+
+  it('calculates a BPM of 0 for 1 tap', () => {
+    const counter = new BpmCounter();
+    counter.tap();
+    expect(counter.bpm).toBe(0);
+  });
+
+  it('intervalTooLong check works', () => {
+    const counter = new BpmCounter([500, 1000, 1500, 2000, 2500]);
+    expect(counter.intervalTooLong(4500)).toBe(true);
+  });
+});
